44c631b765507f2056452b823d0942f25e693013
ariola <ariola.markou@gmail.com>
2023-08-06 22:37:28 +0300

    create cat details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import AllCatsPage from "./pages/AllCatsPage";
+import CatDetailsPage from "./pages/CatDetailsPage";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<h1> Homepage </h1>} />
                 <Route path="/cats" element={<AllCatsPage />} />
+                <Route path="/cats/:id" element={<CatDetailsPage />} />
 
                 <Route path="*" element={<h1> 404 page </h1>} />
             </Routes>
---
+++ src/pages/CatDetailsPage.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function CatDetailsPage() {
+    //destructure id
+    const { id } = useParams();
+    // store data
+    const [cat, setCat] = useState();
+
+    const fetchCat = async () => {
+        try {
+            const response = await fetch(
+                `http://localhost:5005/api/cats/${id}`
+            );
+            if (response.status === 200) {
+                const parsedCat = await response.json();
+                setCat(parsedCat);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    //fetch data
+    useEffect(() => {
+        fetchCat();
+    }, []);
+
+    return cat ? (
+        <div>
+            {" "}
+            <h1>Cat Details </h1>
+            <h3> {cat.name}</h3>
+            <button> Adopt </button>
+        </div>
+    ) : (
+        <h1> Loading </h1>
+    );
+}
+
+export default CatDetailsPage;
