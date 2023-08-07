c67645e5aa1a14711af30515ac81bfd185399f68
Angel Guirao <angeguirao92@gmail.com>
2023-08-07 17:13:06 +0200

    created navbar, homepage and established middlewares for shelterpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import {Route, Routes } from 'react-router-dom'
 import './App.css'
-//import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import Navbar from './components/Navbar';
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import AllHomesPage from './pages/AllHomesPage'
 import HomeDetailsPage from './pages/HomeDetailsPage'
 import NewHomePage from './pages/NewHomePage'
 import UpdateHomePage from './pages/UpdateHomePage'
 import IsPrivate from "./components/IsPrivate";
+
 
 function App() {
 
@@ -16,17 +18,30 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/" element={<HomePage/>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/homes' element={
+        <Route path='/profile' element={
         <IsPrivate>
         <Profile />
         </IsPrivate>
         } />
         <Route path='/homes' element={<AllHomesPage />} />
-        <Route path='/homes/new' element={<NewHomePage />} />
-        <Route path='/homes/:id' element={<HomeDetailsPage />} />
-        <Route path='/homes/:id/edit' element={<UpdateHomePage />} />
+        <Route path='/homes/new' element={
+          <IsPrivate>
+            <NewHomePage />
+          </IsPrivate>
+        } />
+        <Route path='/homes/:id' element={
+         <IsPrivate>
+            <HomeDetailsPage />
+        </IsPrivate>
+        } />
+        <Route path='/homes/:id/edit' element={
+        <IsPrivate>
+            <UpdateHomePage />
+        </IsPrivate>
+        } />
         <Route path="*" element={<h1>404 page</h1>} />
       </Routes>
     </div>
