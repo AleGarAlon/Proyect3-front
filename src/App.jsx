715414a64c65775dd9004af541fa83a04ddd9d2d
ariola <ariola.markou@gmail.com>
2023-08-07 18:05:30 +0300

    create update cat page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import AllCatsPage from "./pages/AllCatsPage";
 import CatDetailsPage from "./pages/CatDetailsPage";
 import NewCat from "./pages/NewCat";
+import UpdateCatPage from "./pages/UpdateCatPage";
 
 function App() {
     return (
@@ -12,7 +13,10 @@ function App() {
                 <Route path="/cats" element={<AllCatsPage />} />
                 <Route path="/cats/:id" element={<CatDetailsPage />} />
                 <Route path="/cats/new" element={<NewCat />} />
-
+                <Route
+                    path="/students/:id/update"
+                    element={<UpdateCatPage />}
+                />
                 <Route path="*" element={<h1> 404 page </h1>} />
             </Routes>
         </>
