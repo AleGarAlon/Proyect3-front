f8ce17ffd6f03f3cb00aa01185d26c5a96e41f5d
Alejandro <alegaralonrgmail.com>
2023-08-09 15:56:39 +0200

    comment on the profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -63,73 +63,94 @@ useEffect(() => {
     }
   };
 
-return (
-    <>
+    return (
+        <>
 
-    <img src={profile.image} alt="profile image" />
-    <h2>{profile.name}'s profile page</h2>
-    
+        <img src={profile.image} alt="profile image" />
+        <h2>{profile.name}'s profile page</h2>
+        
 
-    {profile.cat && profile.cat.length > 0 ? (
-      <div>
-        <p>Your cats for adoption</p>
-        <ul>
-          {profile.cat.map((c) => (
-            <>
-            <div key={c._id}>
-            <p >{c.name}</p>
-            <p>{c.photo}</p>
-            <Link to={`/cats/${c._id}/update`}>Edit Cat</Link>
-            <button onClick={() => handleCatDelete(c._id)}>Delete Cat</button>
+        {profile.cat && profile.cat.length > 0 ? (
+        <div>
+            <p>Your cats for adoption</p>
+            <ul>
+            {profile.cat.map((c) => (
+            
+                <div key={c._id}>
+                <p >{c.name}</p>
+                <p>{c.photo}</p>
+                <Link to={`/cats/${c._id}/update`}>Edit Cat</Link>
+                <button onClick={() => handleCatDelete(c._id)}>Delete Cat</button>
+                </div>
+                
+            ))}
+            </ul>
+        </div>
+        ) : (
+            <p></p>
+        )}
+        <Link to="/cats/new"><button>Add a new cat for adoption</button> </Link>
+
+        {profile.house && profile.house.length > 0 ? (
+            <div>
+                <p>Your houses</p>
+                <ul>
+                {profile.house.map((home) => (
+                    
+                    <ul key={home._id}>
+                    <li >{home.name}</li>
+                    <li>{home.photo}</li>
+                    <Link to={`/homes/${home._id}/edit`}>Edit House</Link>
+                    <button onClick={() => handleHomeDelete(home._id)}>Delete House</button>
+                    </ul>
+                    
+                ))}
+                </ul>
             </div>
-            </>
-          ))}
-        </ul>
-      </div>
-    ) : (
-        <p></p>
-    )}
-    <Link to="/cats/new"><button>Add a new cat for adoption</button> </Link>
+            ) : (
+                <p></p>
+            )}
+        <Link to="/homes/new"><button>Add a new home</button> </Link>  
+        
+        {profile.articles && profile.articles.length > 0 ? (
+            <div>
+                <p>Your articles</p>
+                <ul>
+                {profile.articles.map((article) => (
+                    <>
+                    <ul key={article._id}>
+                    <li >{article.title}</li>
+                    </ul>
+                    </>
+                ))}
+                </ul>
+            </div>
+            ) : (
+                <p></p>
+            )}
+        
+         {profile.comments && profile.comments.length > 0 ? (
+            <div>
+              <p>Your cats for adoption</p>
+              <ul>
+                {profile.comments.map((c) => (
+                  <div key={c._id}>
+                  <Link to ={`/comment/info/${c._id}`}>
+                  <p >{c.title}</p>
+                  <p>{c.author}</p>
+                  </Link>
+                  </div>
+                  
+                  
+                ))}
+              </ul>
+            </div>
+          ) : (
+              <p></p>
+          )}
+        </>  
+    )
 
-    {profile.house && profile.house.length > 0 ? (
-        <div>
-            <p>Your houses</p>
-            <ul>
-            {profile.house.map((home) => (
-                <>
-                <ul key={home._id}>
-                <li >{home.name}</li>
-                <li>{home.photo}</li>
-                <Link to={`/homes/${home._id}/edit`}>Edit House</Link>
-                <button onClick={() => handleHomeDelete(home._id)}>Delete House</button>
-                </ul>
-                </>
-            ))}
-            </ul>
-        </div>
-        ) : (
-            <p></p>
-        )}
-    <Link to="/homes/new"><button>Add a new home</button> </Link>  
-    
-    {profile.articles && profile.articles.length > 0 ? (
-        <div>
-            <p>Your articles</p>
-            <ul>
-            {profile.articles.map((article) => (
-                <>
-                <ul key={article._id}>
-                <li >{article.title}</li>
-                </ul>
-                </>
-            ))}
-            </ul>
-        </div>
-        ) : (
-            <p></p>
-        )}
-    </>
-)
 }
 
 export default Profile;
