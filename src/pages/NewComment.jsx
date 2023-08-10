910df034e34e613299049b0875a87302f03ba84e
ariola <ariola.markou@gmail.com>
2023-08-11 01:47:30 +0300

    style new comments page

--- src/pages/NewComment.jsx
+++ src/pages/NewComment.jsx
@@ -1,28 +1,28 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 import { API_URL } from "../config/config.index";
 import { AuthContext } from "../context/Auth.context";
-import { useContext, useState} from 'react';
+import { useContext, useState } from "react";
+import "./NewComment.css";
 
 const NewComment = () => {
     const navigate = useNavigate();
-    const authContext = useContext(AuthContext); 
+    const authContext = useContext(AuthContext);
     const userEmail = authContext.user.email;
     const [title, setTitle] = useState("");
     const [information, setInformation] = useState("");
     const [author, setAuthor] = useState(userEmail);
     const { id } = useParams();
-    
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
+
         try {
             const payload = {
                 title,
                 information,
-                author : userEmail,
-                receptor : id
-            }
+                author: userEmail,
+                receptor: id,
+            };
             const response = await fetch(`${API_URL}/comments/:id`, {
                 method: "POST",
                 headers: {
@@ -30,11 +30,11 @@ const NewComment = () => {
                 },
                 body: JSON.stringify({ payload }),
             });
-            
+
             if (response.status === 201) {
                 const parsed = await response.json();
                 console.log(parsed);
-                
+
                 navigate(`/profile`);
                 // Logic to empty your state to have a clean form
                 setTitle("");
@@ -44,34 +44,35 @@ const NewComment = () => {
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     return (
-        <>
+        <div className="new-comment">
             <h1>Comment to the owner</h1>
             <form onSubmit={handleSubmit}>
-            <input
-                type='text'
-                value={title}
-                placeholder='Comment title'
-                onChange={e => {
-                    setTitle(e.target.value);
-                }}
-            />
-            
-            <textarea
-                value={information}
-                placeholder=''
-                onChange={e => {
-                    setInformation(e.target.value);
-                }}
-            />
-           
-            <button type='submit'>Post</button>
-        </form>
-        </>
+                <input
+                    type="text"
+                    value={title}
+                    placeholder="Comment title"
+                    onChange={(e) => {
+                        setTitle(e.target.value);
+                    }}
+                />
+
+                <textarea
+                    value={information}
+                    placeholder="Description"
+                    onChange={(e) => {
+                        setInformation(e.target.value);
+                    }}
+                />
+
+                <button type="submit" className="comment-btn">
+                    Post
+                </button>
+            </form>
+        </div>
     );
-    
-}
+};
 
 export default NewComment;
