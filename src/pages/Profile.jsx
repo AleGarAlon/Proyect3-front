6b6cc01ac58f59d71763ada9d53bf31094c56eda
ariola <ariola.markou@gmail.com>
2023-08-10 13:10:30 +0300

    style profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,100 +1,106 @@
-import { useContext, useState, useEffect} from "react";
-import { useNavigate, Link} from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../context/Auth.context";
 import { API_URL } from "../config/config.index";
+import "./Profile.css";
 
-function Profile () {
-const value = useContext(AuthContext)
-const user = value.user
-console.log("User", user)
-const [profile, setProfile] = useState ("")
+function Profile() {
+    const value = useContext(AuthContext);
+    const user = value.user;
+    console.log("User", user);
+    const [profile, setProfile] = useState("");
 
-const fetchUser = async () => {
-  try {
-      const response = await fetch(`${API_URL}/auth/${user._id}`);
-      if (response.status === 200) {
-          const parsed = await response.json();
-          console.log("you fetched info is",parsed)
-          setProfile(parsed);
-      }
-  } catch (error) {
-      console.error(error);
-  }
-};
+    const fetchUser = async () => {
+        try {
+            const response = await fetch(`${API_URL}/auth/${user._id}`);
+            if (response.status === 200) {
+                const parsed = await response.json();
+                console.log("you fetched info is", parsed);
+                setProfile(parsed);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-//fetch data
-useEffect(() => {
-  fetchUser();
-}, []);
+    //fetch data
+    useEffect(() => {
+        fetchUser();
+    }, []);
 
-
-
-
-return (
-    <>
-
-    <img src={profile.image} alt="profile image" />
-    <h2>{profile.name}'s profile page</h2>
-    
-
-    {profile.cat && profile.cat.length > 0 ? (
-      <div>
-        <p>Your cats for adoption</p>
-        <ul>
-          {profile.cat.map((c) => (
-            <>
-            <div key={c._id}>
-            <p >{c.name}</p>
-            <p>{c.photo}</p>
-            </div>
-            </>
-          ))}
-        </ul>
-      </div>
-    ) : (
-        <p></p>
-    )}
-    <Link to="/cats/new"><button>Add a new cat for adoption</button> </Link>
-
-    {profile.house && profile.house.length > 0 ? (
-        <div>
-            <p>Your houses</p>
-            <ul>
-            {profile.house.map((home) => (
-                <>
-                <ul key={home._id}>
-                <Link to="='/homes/"{...home._id}>
-                <li >{home.name}</li>
-                <li>{home.photo}</li>
+    return (
+        <div className="profile-container">
+            <div className="profile-actions">
+                <img src={profile.image} alt="profile image" />
+                <Link to="/cats/new">
+                    <button className="profile-btns">
+                        Add a new cat for adoption
+                    </button>{" "}
                 </Link>
-                </ul>
-                </>
-            ))}
-            </ul>
+                <Link to="/homes/new">
+                    <button className="profile-btns">Add a new home</button>{" "}
+                </Link>
+            </div>
+            <div className="profile-information">
+                <h2>Hey {profile.name}!</h2>
+
+                {profile.cat && profile.cat.length > 0 ? (
+                    <div>
+                        <p>Your cats for adoption</p>
+                        <ul>
+                            {profile.cat.map((c) => (
+                                <>
+                                    <div key={c._id}>
+                                        <p>{c.name}</p>
+                                        <p>{c.photo}</p>
+                                    </div>
+                                </>
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p></p>
+                )}
+
+                {profile.house && profile.house.length > 0 ? (
+                    <div>
+                        <p>Your houses</p>
+                        <ul>
+                            {profile.house.map((home) => (
+                                <>
+                                    <ul key={home._id}>
+                                        <Link to="='/homes/" {...home._id}>
+                                            <li>{home.name}</li>
+                                            <li>{home.photo}</li>
+                                        </Link>
+                                    </ul>
+                                </>
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p></p>
+                )}
+
+                {profile.articles && profile.articles.length > 0 ? (
+                    <div>
+                        <p>Your articles</p>
+                        <ul>
+                            {profile.articles.map((article) => (
+                                <>
+                                    <ul key={article._id}>
+                                        <li>{article.title}</li>
+                                    </ul>
+                                </>
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p></p>
+                )}
+            </div>
         </div>
-        ) : (
-            <p></p>
-        )}
-    <Link to="/homes/new"><button>Add a new home</button> </Link>  
-    
-    {profile.articles && profile.articles.length > 0 ? (
-        <div>
-            <p>Your articles</p>
-            <ul>
-            {profile.articles.map((article) => (
-                <>
-                <ul key={article._id}>
-                <li >{article.title}</li>
-                </ul>
-                </>
-            ))}
-            </ul>
-        </div>
-        ) : (
-            <p></p>
-        )}
-    </>
-)
+    );
 }
 
 export default Profile;
