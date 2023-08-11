fb60939022addca8052c764c8e81ae49f7bece48
ariola <ariola.markou@gmail.com>
2023-08-11 09:22:49 +0300

    style comment detail page

--- src/pages/CommentDetail.jsx
+++ src/pages/CommentDetail.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { API_URL } from "../config/config.index";
+import "./CommentDetail.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 function CommentDetails() {
     //destructure id
@@ -15,10 +18,9 @@ function CommentDetails() {
             const response = await fetch(`${API_URL}/comments/info/${id}`);
             if (response.status === 200) {
                 const parsed = await response.json();
-                console.log("your parsed is" ,parsed)
-                
+                console.log("your parsed is", parsed);
+
                 setComment(parsed);
-                
             }
         } catch (error) {
             console.error(error);
@@ -44,12 +46,37 @@ function CommentDetails() {
 
     // will be undefined, so we use Conditional (ternary)
     return comment ? (
-        <div>
-            <h1>Comment Details</h1>
-            <h3>Name: {comment.title}</h3>
-            <p>Author:{comment.author} </p>
-            <p>Description: {comment.information}</p>
-            <button onClick={handleDelete}>Delete</button>
+        <div className="comment-detail-container">
+            <div className="comment-intro">
+                <h1>Comment Details</h1>
+            </div>
+            <div className="comment-details">
+                <div className="comment-detail">
+                    <h3>
+                        Name {comment.title}{" "}
+                        <FontAwesomeIcon
+                            icon={faPaw}
+                            size="lg"
+                            style={{ color: "#fafcff" }}
+                        />
+                    </h3>
+                    <h3>
+                        Author
+                        <p>{comment.author} </p>
+                    </h3>
+                    <h3>
+                        {" "}
+                        Description
+                        <p>{comment.information}</p>{" "}
+                    </h3>
+                    <button
+                        onClick={handleDelete}
+                        className="comment-detail-btn"
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
         </div>
     ) : (
         <h1>Wait... we have a furball...</h1>
